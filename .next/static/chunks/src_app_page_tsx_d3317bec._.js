4e372ce9d9eea1f980893abe5d4ca8c0379c2346
aquariumlumii <aquariumlumii@gmail.com>
2025-08-20 12:30:18 +0530

    major changes

--- .next/static/chunks/src_app_page_tsx_d3317bec._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_react-responsive-carousel_lib_styles_carousel_min_5075f2c6.css",
-  "static/chunks/node_modules_709c8ced._.js",
-  "static/chunks/src_859f597d._.js"
-],
-    source: "dynamic"
-});

2e3ee31d3c742386b678b1f3101df2347c703440
aquariumlumii <aquariumlumii@gmail.com>
2025-08-25 12:28:38 +0530

    minor updates

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,7 +9,7 @@ const ContactUsPage: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
 
         <div className="text-center mb-16">
-          <h1 className="text-3xl font-extrabold text-white mb-4 leading-tight">
+          <h1 className="text-4xl font-extrabold text-white mb-4 leading-tight">
             Get in Touch
           </h1>
           <p className="text-lg text-gray-400 max-w-3xl mx-auto">
